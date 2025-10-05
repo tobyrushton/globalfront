07f3a9deed288d4444e3d1f5626a1f6058235d45
tobyrushton <tobyjrushton@icloud.com>
2025-10-05 16:40:08 +0100

    client: add join button

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -3,10 +3,18 @@ import { getCurrentGame } from '@/lib/matchmaker'
 
 const Home = async () => {
   const game = await getCurrentGame()
+  const gameObj = game.toObject()
 
   return (
-    <div>
-
+    <div className='absolute flex w-full h-full'>
+      <div className="flex w-full h-full justify-center items-center">
+        <button className='flex p-2 rounded-xl border-white cursor-pointer border-2'>
+          <div className='flex flex-col'>
+            <p>game: {gameObj.id}</p>
+            <p>{gameObj.player_count} / {gameObj.max_players}</p>
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
