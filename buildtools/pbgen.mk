08e45562c4390040d6857cc8894434990c465f54
tobyrushton <tobyjrushton@icloud.com>
2025-10-06 01:09:10 +0100

    pb: generate grpc-web files

--- buildtools/pbgen.mk
+++ buildtools/pbgen.mk
@@ -1,13 +1,22 @@
 .PHONY: pbgen pbgen-mm pbgen-game
 
-pbgen-mm: 
-	protoc --go_out=../pb --go_opt=paths=source_relative \
-    --go-grpc_out=../pb --go-grpc_opt=paths=source_relative \
-    --ts_out=../pb --ts_opt=es6,import_style=commonjs,binary \
-    --proto_path=../proto  matchmaker/v1/matchmaker.proto
+PROTO_DIR := ../proto
+OUT_DIR := ../pb
+
+pbgen: pbgen-mm pbgen-game
+
+pbgen-mm:
+	protoc --proto_path=$(PROTO_DIR) \
+		matchmaker/v1/matchmaker.proto \
+		--go_out=$(OUT_DIR) --go_opt=paths=source_relative \
+		--go-grpc_out=$(OUT_DIR) --go-grpc_opt=paths=source_relative \
+		--ts_out=$(OUT_DIR) --ts_opt=es6,import_style=commonjs,binary \
+		--grpc-web_out=import_style=typescript,mode=grpcwebtext:$(OUT_DIR)
 
 pbgen-game:
-	protoc --go_out=../pb --go_opt=paths=source_relative \
-    --go-grpc_out=../pb --go-grpc_opt=paths=source_relative \
-    --ts_out=../pb --ts_opt=es6,import_style=commonjs,binary \
-    --proto_path=../proto  game/v1/game.proto
+	protoc --proto_path=$(PROTO_DIR) \
+		game/v1/game.proto \
+		--go_out=$(OUT_DIR) --go_opt=paths=source_relative \
+		--go-grpc_out=$(OUT_DIR) --go-grpc_opt=paths=source_relative \
+		--ts_out=$(OUT_DIR) --ts_opt=es6,import_style=commonjs,binary \
+		--grpc-web_out=import_style=typescript,mode=grpcwebtext:$(OUT_DIR)
