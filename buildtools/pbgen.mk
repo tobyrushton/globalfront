7545a3f456e39286114ca54683de5ddc95202a8a
tobyrushton <tobyjrushton@icloud.com>
2025-10-06 14:46:20 +0100

    proto: define gamebox schema

--- buildtools/pbgen.mk
+++ buildtools/pbgen.mk
@@ -1,9 +1,9 @@
-.PHONY: pbgen pbgen-mm pbgen-game pbgen-web
+.PHONY: pbgen pbgen-mm pbgen-game pbgen-web pbgen-gb
 
 PROTO_DIR := ../proto
 OUT_DIR := ../pb
 
-pbgen: pbgen-mm pbgen-game pbgen-web
+pbgen: pbgen-mm pbgen-game pbgen-gb
 
 pbgen-mm:
 	protoc --proto_path=$(PROTO_DIR) \
@@ -19,9 +19,8 @@ pbgen-game:
 		--go-grpc_out=$(OUT_DIR) --go-grpc_opt=paths=source_relative \
 		--ts_out=$(OUT_DIR)
 
-# pbgen-web:
-# 	protoc --proto_path=$(PROTO_DIR) \
-# 		matchmaker/v1/matchmaker.proto \
-# 		game/v1/game.proto \
-# 		--js_out=import_style=commonjs,binary:$(OUT_DIR) \
-# 		--grpc-web_out=import_style=typescript,mode=grpcwebtext:$(OUT_DIR)
+pbgen-gb:
+	protoc --proto_path=$(PROTO_DIR) \
+		gamebox/v1/gamebox.proto \
+		--go_out=$(OUT_DIR) --go_opt=paths=source_relative \
+		--go-grpc_out=$(OUT_DIR) --go-grpc_opt=paths=source_relative
