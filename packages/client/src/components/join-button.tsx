420117539a18e38959470eecd67662795681474f
tobyrushton <tobyjrushton@icloud.com>
2025-10-06 23:36:20 +0100

    client: push user to game page when created

--- packages/client/src/components/join-button.tsx
+++ packages/client/src/components/join-button.tsx
@@ -3,20 +3,26 @@
 import { FC } from "react"
 import { Game } from "@globalfront/pb/game/v1/game"
 import { webClient } from "@/grpc/webClient"
+import { useRouter } from "next/navigation"
 
 type JoinButtonProps = {
     game: Game
 }
 
-const joinGame = () => {
-    const stream = webClient.joinGame({})
-
-    stream.responses.onNext((response) => {
-        console.log(response)
-    })
-}
-
 export const JoinButton: FC<JoinButtonProps> = ({ game }) => {
+    const router = useRouter()
+
+    const joinGame = () => {
+        const stream = webClient.joinGame({})
+
+        stream.responses.onNext((response) => {
+            if (response) {
+                if (response.update.oneofKind === "serverDetails") 
+                    router.push(`/game/${response.update.serverDetails.id}`)
+            }
+        })
+    }
+
     return (
         <button 
             className='flex p-2 rounded-xl border-white cursor-pointer border-2'
