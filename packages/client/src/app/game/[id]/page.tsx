5d355378a16e4c83cfebaa532013ae49f495d44a
tobyrushton <tobyjrushton@icloud.com>
2025-10-07 16:46:17 +0100

    client: init GameBoard

--- packages/client/src/app/game/[id]/page.tsx
+++ packages/client/src/app/game/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { GameWrapper } from "@/components/game/wrapper";
+import { GameBoard } from "@/components/game/board";
 
 const GamePage: FC = () => {
     // do something to fetch the url here
     return (
         <GameWrapper>
-            <div>Game Page</div>
+            <GameBoard />
         </GameWrapper>
     )    
 }
---
+++ packages/client/src/components/game/board.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+export const GameBoard: FC = () => {
+    return (
+        <div className="w-[2000px] h-[2000px] bg-green-500/20 border border-green-500 relative">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-green-500/50 border border-green-500 flex justify-center items-center">
+                <span className="text-white font-bold">Center</span>
+            </div>
+        </div>
+    )
+}
