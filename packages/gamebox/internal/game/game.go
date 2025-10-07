5069f82e55d45d5b66ab59716427918dac85ac51
tobyrushton <tobyjrushton@icloud.com>
2025-10-07 16:10:35 +0100

    gamebox/ws: setup ws server for each game

--- packages/gamebox/internal/game/game.go
+++ packages/gamebox/internal/game/game.go
@@ -1,23 +1,35 @@
 package game
 
 import (
+	"fmt"
+	"net/http"
+
+	ws "github.com/tobyrushton/globalfront/packages/gamebox/internal/ws/server"
 	pb "github.com/tobyrushton/globalfront/pb/game/v1"
 )
 
 type Game struct {
 	port int
 	game *pb.Game
+
+	wsServer *ws.WsServer
 }
 
 func New(port int, game *pb.Game) *Game {
 	return &Game{
-		port: port,
-		game: game,
+		port:     port,
+		game:     game,
+		wsServer: ws.NewServer(),
 	}
 }
 
 func (g *Game) Start() error {
-	return nil
+	http.HandleFunc("/ws", g.wsServer.ServeWS)
+
+	return http.ListenAndServe(
+		fmt.Sprintf(":%d", g.port),
+		nil,
+	)
 }
 
 func (g *Game) GetId() string {
